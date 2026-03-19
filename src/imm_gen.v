1f1f15644e544bf16b3c5ec32b93b70cc6444ea1
Youngho Cho <dominoyh5@gmail.com>
2026-03-19 15:08:00 +0900

    refactor: imm_gen index range

--- src/imm_gen.v
+++ src/imm_gen.v
@@ -8,11 +8,12 @@ module imm_gen (
 
     always @(*) begin
         case (type)
-            `I_TYPE: imm = { {21{inst[31]}}, inst[30:25], inst[24:21], inst[20] };
-            `S_TYPE: imm = { {21{inst[31]}}, inst[30:25], inst[11:8], inst[7] };
-            `U_TYPE: imm = { inst[31], inst[30:20], inst[19:12], {12{1'b0}} };
-            `B_TYPE: imm = { {20{inst[31]}}, inst[7], inst[30:25], inst[11:8], 1'b0 };
-            `J_TYPE: imm = { {12{inst[31]}}, inst[19:12], inst[20], inst[30:25], inst[24:21], 1'b0};
+            `I_TYPE: imm = { {20{inst[31]}}, inst[31:20] };
+            `S_TYPE: imm = { {20{inst[31]}}, inst[31:25], inst[11:7] };
+            `B_TYPE: imm = { {19{inst[31]}}, inst[31], inst[7], inst[30:25], inst[11:8], 1'b0 };
+            `U_TYPE: imm = { inst[31:12], 12'b0 };
+            `J_TYPE: imm = { {11{inst[31]}}, inst[31], inst[19:12], inst[20], inst[30:21], 1'b0 };
+            default: imm = 32'b0;
         endcase
     end
 
