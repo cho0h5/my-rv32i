837c68ca8069d4450a326cde9c405a35d00108a6
Youngho Cho <dominoyh5@gmail.com>
2026-03-19 15:07:40 +0900

    fix: init regfile

--- src/regfile.v
+++ src/regfile.v
@@ -8,6 +8,13 @@ module regfile (
 
     reg [31:0] regs [1:31];
 
+    integer i;
+    initial begin
+        for (i = 1; i < 32; i = i + 1) begin
+            regs[i] = 32'b0;
+        end
+    end
+
     assign rdata1 = rs1 == 5'd0 ? 32'd0 : regs[rs1];
     assign rdata2 = rs2 == 5'd0 ? 32'd0 : regs[rs2];
 
